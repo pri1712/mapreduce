580928c6fda0af028118015fc5bed39e8587fc64
Priyanshu Soni <priyanshu@Priyanshus-MacBook-Air.local>
2025-07-27 14:10:03 +0530

    initial commit

---
+++ src/mr/coordinator.go
@@ -0,0 +1,222 @@
+package mr
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+import "net"
+import "os"
+import "net/rpc"
+import "net/http"
+
+type TaskPhase int
+type TaskStatus int
+
+const (
+	MapPhase TaskPhase = iota
+	ReducePhase
+	DonePhase
+)
+
+const (
+	Idle TaskStatus = iota
+	InProgress
+	Completed
+)
+
+type TaskInfo struct {
+	TaskId     int
+	TaskType   TaskPhase  //map,reduce or done
+	TaskStatus TaskStatus // done,inprogress or idle
+	file       string
+	StartTime  time.Time
+	workerID   int
+	InputFiles []string
+	OutputFile string
+}
+
+type Coordinator struct {
+	mutex             sync.Mutex
+	files             []string
+	nMap              int
+	nReduce           int
+	done              bool
+	phase             TaskPhase
+	TaskQueue         chan int //denoted by the taskid.
+	MapTasks          map[int]*TaskInfo
+	ReduceTasks       map[int]*TaskInfo
+	MapTasksCompleted int
+}
+
+type RequestTaskArgs struct {
+	WorkerID int
+}
+
+type RequestTaskReply struct {
+	TaskId      int
+	TaskType    TaskPhase
+	MapFile     string
+	ReduceFiles []string //input to reduce tasks are a bunch of files, to be precise 1 per mapper
+	ReduceId    int
+	Dummy       bool //to check if valid data is sent or not.
+}
+
+type MapTaskCompletionArgs struct {
+	WorkerID int
+	TaskId   int
+}
+
+type TaskCompletionReply struct {
+	Recorded bool
+}
+
+func (c *Coordinator) UpdateTaskState(workerID int, info *TaskInfo) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	info.workerID = workerID
+	info.TaskStatus = InProgress
+	info.StartTime = time.Now()
+	return
+}
+
+func (c *Coordinator) RequestMapTask(args *RequestTaskArgs, reply *RequestTaskReply) error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	select {
+	case NextTaskId := <-c.TaskQueue:
+		task := c.MapTasks[NextTaskId]
+		if task.TaskStatus != Idle {
+			reply.Dummy = false //to show that its an invalid task.
+			return nil
+		}
+		c.UpdateTaskState(args.WorkerID, task) //updates internal state of the task.
+		//send using rpc now to the worker with workerid given in args.
+		reply.TaskId = task.TaskId
+		reply.TaskType = task.TaskType
+		reply.MapFile = task.file
+		reply.Dummy = true
+	default:
+		reply.Dummy = false
+	}
+	return nil
+}
+
+func (c *Coordinator) ReportMapTaskCompletion(args *MapTaskCompletionArgs, reply *TaskCompletionReply) error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	task, exists := c.MapTasks[args.TaskId]
+	if !exists {
+		log.Printf("Invalid TaskId %d reported by worker %d", args.TaskId, args.WorkerID)
+		reply.Recorded = false
+		return nil
+	}
+	if task.TaskStatus != Completed {
+		task.TaskStatus = Completed
+		log.Print("Report MapTaskCompletion completed")
+		log.Print("Time taken: ", time.Now().Sub(task.StartTime))
+		reply.Recorded = true
+		c.MapTasksCompleted++
+		if checkMapCompletion(c.MapTasksCompleted, len(c.files)) {
+			//finish the map phase, update the phase to reduce phase.
+
+		}
+	}
+	return nil
+}
+
+func checkMapCompletion(completed int, total int) bool {
+	if completed == total {
+		return true
+	}
+	return false
+}
+
+func CheckDeadWorkers(info map[int]*TaskInfo) []int {
+	//find all deadworkers.
+	var deadWorkers []int
+	for _, maptask := range info {
+		if maptask.TaskStatus == InProgress && time.Since(maptask.StartTime).Seconds() > 10 {
+			deadWorkers = append(deadWorkers, maptask.workerID)
+		}
+	}
+	return deadWorkers
+}
+
+func (c *Coordinator) HandleDeadWorkers(deadWorkers []int) {
+	//manage all the tasks which have the deadworker as the workerID.
+	for _, workerID := range deadWorkers {
+		for taskID, task := range c.MapTasks {
+			if task.workerID == workerID && task.TaskStatus == InProgress {
+				task.TaskStatus = Idle
+				task.workerID = -1
+				c.TaskQueue <- taskID
+			}
+		}
+	}
+}
+
+// an example RPC handler.
+// the RPC argument and reply types are defined in rpc.go
+func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
+	reply.Y = args.X + 1
+	return nil
+}
+
+// start a thread that listens for RPCs from worker.go
+func (c *Coordinator) server() {
+	rpc.Register(c)
+	rpc.HandleHTTP()
+	//l, e := net.Listen("tcp", ":1234")
+	sockname := coordinatorSock()
+	os.Remove(sockname)
+	l, e := net.Listen("unix", sockname)
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	go http.Serve(l, nil)
+}
+
+// main/mrcoordinator.go calls Done() periodically to find out
+// if the entire job has finished.
+func (c *Coordinator) Done() bool {
+	ret := false
+
+	//check if the all the map and reduce tasks are done, then return true.
+
+	return ret
+}
+
+// nReduce is the number of reduce tasks to use.
+func MakeCoordinator(files []string, nReduce int) *Coordinator {
+	c := Coordinator{}
+	c.nReduce = nReduce
+	c.files = files
+	c.nMap = len(files) //number of map tasks.
+	c.done = false
+	c.phase = MapPhase
+	c.MapTasks = make(map[int]*TaskInfo, len(files))
+	for i, file := range files {
+		t := TaskInfo{}
+		t.file = file
+		t.TaskId = i
+		t.TaskType = MapPhase
+		t.TaskStatus = Idle
+		t.StartTime = time.Time{} //zero value
+		c.MapTasks[i] = &t
+		c.TaskQueue <- i //inserting into task channel.
+	}
+	go func() {
+		for {
+			time.Sleep(2 * time.Second)
+			c.mutex.Lock()
+			deadWorkers := CheckDeadWorkers(c.MapTasks)
+			c.HandleDeadWorkers(deadWorkers)
+			c.mutex.Unlock()
+		}
+	}()
+	c.server()
+	return &c
+}
